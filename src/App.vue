5a1dcb0c832b1422ed43c670bb15607efef96a33
Matt Schultz <matt@mwschultz.com>
2024-12-28 16:52:30 -0500

    Setting up:

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Private Schools Data</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">{{ error }}</div>
+    <pre v-else>{{ formattedData }}</pre>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      data: null,
+      loading: true,
+      error: null,
+    };
+  },
+  computed: {
+    formattedData() {
+      return JSON.stringify(this.data, null, 2);
+    },
+  },
+  async created() {
+    try {
+      await this.fetchToken();
+      await this.fetchData();
+    } catch (error) {
+      this.error = "Failed to fetch data";
+    } finally {
+      this.loading = false;
+    }
+  },
+  methods: {
+    async fetchToken() {
+      try {
+        const response = await axios.post(
+          "https://rails-apis.us.auth0.com/oauth/token",
+          {
+            client_id: process.env.VUE_APP_NC_PRIVATE_SCHOOLS_ID,
+            client_secret: process.env.VUE_APP_NC_PRIVATE_SCHOOLS_CLIENT_SECRET,
+            audience: process.env.VUE_APP_NC_PRIVATE_SCHOOLS_CLIENT_AUDIENCE,
+            grant_type: process.env.VUE_APP_NC_PRIVATE_SCHOOLS_GRANT_TYPE,
+          },
+          {
+            headers: {
+              "content-type": "application/json",
+            },
+          }
+        );
+        this.token = response.data.access_token;
+      } catch (error) {
+        this.error = "Failed to fetch token";
+        throw error;
+      }
+    },
+
+    async fetchData() {
+      try {
+        const response = await axios.get("https://private-schools.onrender.com/api/v1/counties/", {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        });
+        this.data = response.data;
+      } catch (error) {
+        this.error = "Failed to fetch data";
+        throw error;
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+pre {
+  text-align: left;
+  background: #f4f4f4;
+  padding: 10px;
+  border-radius: 5px;
+  overflow: auto;
 }
 </style>
